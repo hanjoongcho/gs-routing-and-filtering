961295fc4659d1a28b2366c13e92cfdee7a7fc5e
hanjoongcho <hanjoong-cho@unimewo.com>
2018-04-18 17:26:57 +0900

    Added test code for the CURL post parameter

--- complete/book/src/main/java/hello/BookApplication.java
+++ complete/book/src/main/java/hello/BookApplication.java
@@ -1,5 +1,9 @@
 package hello;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class BookApplication {
-
-  @RequestMapping(value = "/available")
-  public String available() {
-    return "Spring in Action";
-  }
-
-  @RequestMapping(value = "/checked-out")
-  public String checkedOut() {
-    return "Spring Boot in Action";
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(BookApplication.class, args);
-  }
+	private static Logger log = LoggerFactory.getLogger(BookApplication.class);
+	
+	@RequestMapping(value = "/available")
+	public String available(HttpServletRequest request) {
+		// curl -d "testParam=apple" -X POST  192.168.56.101:8080/books/available
+	    log.info(String.format("%s request parameter %s", request.getMethod(), request.getParameter("testParam")));
+		return "Spring in Action";
+	}
+	
+	@RequestMapping(value = "/checked-out")
+	public String checkedOut() {
+		return "Spring Boot in Action";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BookApplication.class, args);
+	}
 }
